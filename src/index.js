228069c176ebfd20011a82cb3f5aa95e414f343f
Divya Patel <drew@ninjatechnolabs.com>
2022-12-21 19:36:06 +0530

    initial push

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./routes/root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./routes/error-page";
+import { SearchMentors } from "./routes/searchMentors/searchMentors";
+
+import "./scss/custom.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "search-mentors",
+        element: <SearchMentors />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
